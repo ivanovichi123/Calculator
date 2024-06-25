a33cb6d137abd14ff010d49e6e57e8d9e3cd2734
ivanovichi <ivan.arthur18@gmail.com>
2024-06-25 16:24:34 -0600

    Functions and making the page
    I add 4 functions for basics operations, add, minus, multiply and divide.
    Also add the buttons necessary for the calculator

---
+++ javascript.js
@@ -0,0 +1,40 @@
+let firstNumber = 2;
+let secondNumber = 3;
+let operator = "+";
+
+const oper = function operate(firstNumber, secondNumber, operator) {
+    if (operator === "+"){
+        add(firstNumber, secondNumber);
+    } else {
+        console.log("a");
+    }
+}
+
+const add = function sum(a, b) {
+    let op = a + b;
+    console.log(op);
+}
+
+const minus = function subtract(a, b) {
+    let op2 = a - b;
+    console.log(op2)
+}
+
+const mul = function multiply(a, b) {
+    let op3 = a * b;
+    console.log(op3);
+}
+
+const div = function divide(a, b) {
+    let op4 = a/b;
+    console.log(op4);
+}
+
+
+
+
+add(1, 2);
+minus(2,1);
+mul(2, 2);
+div(6, 3);
+oper(firstNumber, secondNumber, operator);
